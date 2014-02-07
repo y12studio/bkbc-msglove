bf9429b755e037200e6d6376626aaf43d886c710
y12studio <y12studio@gmail.com>
2014-02-07 19:17:41 +0800

    fix #5

--- android/src/org/blackbananacoin/msglove/MainActivity.java
+++ android/src/org/blackbananacoin/msglove/MainActivity.java
@@ -20,19 +20,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.blackbananacoin.common.bitcoin.Bitcoins;
-
-import com.google.bitcoin.core.Address;
-import com.google.bitcoin.core.AddressFormatException;
-import com.google.bitcoin.core.DumpedPrivateKey;
-import com.google.bitcoin.core.ECKey;
-import com.google.bitcoin.params.MainNetParams;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -48,6 +39,10 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.params.MainNetParams;
+
 public class MainActivity extends Activity implements OnClickListener,
 		TextWatcher {
 
@@ -58,27 +53,6 @@ public class MainActivity extends Activity implements OnClickListener,
 	private EditText edMsgLove;
 	private EditText edBtcAmount;
 	private TextView tvBtcAddr;
-
-	private static class KeyInfo {
-		private Address addr;
-		private DumpedPrivateKey privateKeyEncoded;
-
-		public Address getAddr() {
-			return addr;
-		}
-
-		public void setAddr(Address addr) {
-			this.addr = addr;
-		}
-
-		public DumpedPrivateKey getPrivateKeyEncoded() {
-			return privateKeyEncoded;
-		}
-
-		public void setPrivateKeyEncoded(DumpedPrivateKey privateKeyEncoded) {
-			this.privateKeyEncoded = privateKeyEncoded;
-		}
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -110,39 +84,6 @@ public class MainActivity extends Activity implements OnClickListener,
 		return true;
 	}
 
-	private KeyInfo createKeyInfo(String seed) throws NoSuchAlgorithmException,
-			UnsupportedEncodingException, AddressFormatException {
-		KeyInfo r = new KeyInfo();
-		MessageDigest d256 = MessageDigest.getInstance("SHA-256");
-		byte[] d1 = d256.digest(seed.getBytes("UTF-8"));
-		d256.reset();
-		byte[] privKeyBytes = d256.digest(d1);
-		ECKey eckey = new ECKey(new BigInteger(1, privKeyBytes));
-		DumpedPrivateKey dkey = eckey.getPrivateKeyEncoded(MainNetParams.get());
-		Log.v(UI.TAG, "dump=" + dkey.toString());
-		Address addr = new Address(MainNetParams.get(), eckey.getPubKeyHash());
-		Log.v(UI.TAG, "Addr=" + addr.toString());
-		r.setAddr(addr);
-		//r.setPrivateKey(eckey.getPrivKeyBytes());
-		r.setPrivateKeyEncoded(dkey);
-		testDumpKey(dkey.toString());
-		return r;
-	}
-
-	private void testDumpKey(String input) {
-		try {
-			final ECKey key = new DumpedPrivateKey(MainNetParams.get(), input)
-					.getKey();
-			final Address address = new Address(MainNetParams.get(),
-					key.getPubKeyHash());
-
-			Log.v(UI.TAG, "[testDump]Addr=" + address.toString());
-
-		} catch (final AddressFormatException x) {
-			x.printStackTrace();
-		}
-	}
-
 	public void onClick(View v) {
 		String addr;
 		double amount;
@@ -169,6 +110,7 @@ public class MainActivity extends Activity implements OnClickListener,
 			break;
 		case R.id.btnReset:
 			edMsgLove.setText(UI.MSG);
+			// showSomeBitcoinIssueInfo();
 			break;
 		default:
 			break;
@@ -190,11 +132,11 @@ public class MainActivity extends Activity implements OnClickListener,
 		Log.i(UI.TAG, "timeKey=" + timeInKey);
 
 		String keyFileName = fmFileName.format(new Date());
-		KeyInfo ki = getKeyInfoFromUi();
-		if (ki != null) {
+		ECKey k = getKeyInfoFromUi();
+		if (k != null) {
 			String keyContent = "# KEEP YOUR PRIVATE KEYS SAFE !\n"
-					+ ki.getPrivateKeyEncoded().toString() + " "
-					+ timeInKey + "\n# End of private keys";
+					+ k.getPrivateKeyEncoded(MainNetParams.get()).toString() + " " + timeInKey
+					+ "\n# End of private keys";
 			Log.i(UI.TAG, "keycontent=" + keyContent);
 			if (isExternalStorageWritable()) {
 				File f = getDownloadStorageDir(keyFileName);
@@ -210,27 +152,27 @@ public class MainActivity extends Activity implements OnClickListener,
 			}
 		}
 	}
-	
-	private void handleIncomeIntent(){
+
+	private void handleIncomeIntent() {
 		// Get intent, action and MIME type
-	    Intent intent = getIntent();
-	    String action = intent.getAction();
-	    String type = intent.getType();
-	    if (Intent.ACTION_SEND.equals(action) && type != null) {
-	        if ("text/plain".equals(type)) {
-	            handleSendText(intent); // Handle text being sent
-	        }
-	    } else {
-	        // Handle other intents, such as being started from the home screen
-	    }
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+		if (Intent.ACTION_SEND.equals(action) && type != null) {
+			if ("text/plain".equals(type)) {
+				handleSendText(intent); // Handle text being sent
+			}
+		} else {
+			// Handle other intents, such as being started from the home screen
+		}
 	}
 
 	private void handleSendText(Intent intent) {
 		String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-		if(sharedText!=null){
-			edMsgLove.setText(sharedText);			
+		if (sharedText != null) {
+			edMsgLove.setText(sharedText);
 		}
-		
+
 	}
 
 	public File getDownloadStorageDir(String keyFileName) {
@@ -261,16 +203,27 @@ public class MainActivity extends Activity implements OnClickListener,
 	}
 
 	public void afterTextChanged(Editable s) {
-		KeyInfo ki = getKeyInfoFromUi();
-		if (ki != null) {
-			Address addr = ki.getAddr();
+		ECKey k = getKeyInfoFromUi();
+		if (k != null) {
+			Address addr = k.toAddress(MainNetParams.get());
 			// Log.i(UI.TAG, "addr=" + addr.toString());
 			tvBtcAddr.setText(addr.toString());
 		}
 	}
 
-	public KeyInfo getKeyInfoFromUi() {
-		KeyInfo ki = null;
+	public void showSomeBitcoinIssueInfo() {
+		// 1CounterpartyXXXXXXXXXXXXXXXUWLpVr
+		edL.setText("XCP");
+		edO.setText("OP_RETURN");
+		edV.setText("Proof-of-Burn");
+		edE.setText("^_^");
+		edMsgLove.setText("-L-燒錢想從-O-找-V-上台-E-看戲");
+		edBtcAmount.setText("2,130.83744257");
+		tvBtcAddr.setText("1CounterpartyXXXXXXXXXXXXXXXUWLpVr");
+	}
+
+	public ECKey getKeyInfoFromUi() {
+		ECKey k = null;
 		String tL = edL.getText().toString();
 		String tO = edO.getText().toString();
 		String tV = edV.getText().toString();
@@ -280,16 +233,11 @@ public class MainActivity extends Activity implements OnClickListener,
 				.replace("-V-", tV).replace("-E-", tE);
 		// Log.i(UI.TAG, "msglove=" + msgLove);
 		try {
-			ki = createKeyInfo(msgLove);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+			k = Bitcoins.createECKeyByDoubleDigestString(msgLove);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
-		} catch (AddressFormatException e) {
-			Log.e(UI.TAG, "error=" + e.getMessage());
-			e.printStackTrace();
 		}
-		return ki;
+		return k;
 	}
 
 	public void beforeTextChanged(CharSequence s, int start, int count,
